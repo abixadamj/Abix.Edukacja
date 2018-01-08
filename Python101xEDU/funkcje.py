ef8cff4e93fc2c130916912c6bfa74fdc27a737c
ABIX Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2018-01-08 19:19:27 +0100

    podstawy funkcji

---
+++ Python101xEDU/funkcje.py
@@ -0,0 +1,15 @@
+
+# Uwaga ! pierwsza funkcja wyświetla wynik na ekranie, a druga przekazuje wartość.
+
+def nazwa(a,b):
+    print(a+b)
+
+# Druga funkcja ma też samoopisujacy sie kod.
+
+def nazwa2(a,b):
+    '''
+    To jest moja dokumentacja
+    a,b - wartości całkowite
+    zwraca sumę a+b
+    '''
+    return a+b
