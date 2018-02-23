e11a523925505d170cfe77e1a5c5174c9a3e2fa2
ABIX Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2018-02-23 16:00:58 +0100

    Kod gry
    Tu będzie kod gry, przez weekend napiszę.

--- Webinar_27_02.py
+++ Webinar_27_02.py
@@ -1,1 +1,7 @@
-#!/usr bin/env python3
+#!/usr/bin/env python3
+#
+# Kod gry, którą będziemy tworzyć w oparciu o bibliotekę pyGame ZERO : https://pypi.python.org/pypi/pgzero/
+# dokumentacja biblioteki: https://pygame-zero.readthedocs.io/en/stable/
+# System, w którym już wszystko mamy: https://free-desktop.pl
+#
+#
